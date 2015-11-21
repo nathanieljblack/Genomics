cbd3639be3c58cf2238a5ed26e6eb10573ea3a67
Nathaniel Black <nathaniel.black@ischool.berkeley.edu>
2015-11-21 17:44:49 -0500

    added viz2 and disabled heatmap for now

--- js/viz2.js
+++ js/viz2.js
@@ -1,345 +1,142 @@
-var tip = d3.tip()
-          .attr('class', 'd3-tip')
-          .offset([-5,0])
-          .html(function(d) {
-            return "<a href=" + d.link + " style='text-decoration: none' target='_blank'><span style='color:" + d.color + "'><strong><center>Click here for more info on the " + d.type + "</center></strong></span></a>";
-          });
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var leber = [
-  {
-    "name": "SOME HEATMAP",
-    "parent": "null",
-    "color": "#7f7f7f",
-    "lightcolor": "#c7c7c7",
-    "type": "disease",
-    "link": "https://en.m.wikipedia.org/wiki/Leber's_hereditary_optic_neuropathy",
-    "children": [
-      {
-        "name": "CYTB",
-        "parent": "Leber's Optic Atrophy",
-        "color": "#1f77b4",
-        "lightcolor": "#aec7e8",
-        "type": "gene",
-        "link": "https://en.m.wikipedia.org/wiki/Cytochrome_b",
-        "children": [
-          {
-            "name": "chrMT-15257-A",
-            "parent": "CYTB",
-            "color": "#1f77b4",
-            "lightcolor": "#aec7e8",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/41518645"
-          },
-          {
-            "name": "chrMT-15812-A",
-            "parent": "CYTB",
-            "color": "#1f77b4",
-            "lightcolor": "#aec7e8",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/200336777"
-          },
-          {
-            "name": "chrMT-14831-A",
-            "parent": "CYTB",
-            "color": "#1f77b4",
-            "lightcolor": "#aec7e8",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/199795644"
-          }
-        ]
-      },
-      {
-        "name": "ND1",
-        "parent": "Leber's Optic Atrophy",
-        "color": "#ff7f0e",
-        "lightcolor": "#ffbb78",
-        "type": "gene",
-        "link": "https://en.m.wikipedia.org/wiki/MT-ND1",
-        "children": [
-          {
-            "name": "chrMT-4216-C",
-            "parent": "ND1",
-            "color": "#ff7f0e",
-            "lightcolor": "#ffbb78",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/1599988"
-          },
-          {
-            "name": "chrMT-3394-C",
-            "parent": "ND1",
-            "color": "#ff7f0e",
-            "lightcolor": "#ffbb78",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/41460449"
-          },
-          {
-            "name": "chrMT-4136-G",
-            "parent": "ND1",
-            "color": "#ff7f0e",
-            "lightcolor": "#ffbb78",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/199476121"
-          }]},
-           {
-          "name": "ND2",
-          "parent": "Leber's Optic Atrophy",
-          "color": "#2ca02c",
-          "lightcolor": "#98df8a",
-          "type": "gene",
-          "link": "https://en.m.wikipedia.org/wiki/MT-ND2",
-          "children": [
-            {
-              "name": "chrMT-4917-G",
-              "parent": "ND2",
-              "color": "#2ca02c",
-              "lightcolor": "#98df8a",
-              "type": "mutation",
-              "link": "http://www.ncbi.nlm.nih.gov/snp/28357980"
-            }
-            ]},
-            {
-          "name": "ND3",
-          "parent": "Leber's Optic Atrophy",
-          "color": "#8c564b",
-          "lightcolor": "#c49c94",
-          "type": "gene",
-          "link": "https://en.m.wikipedia.org/wiki/MT-ND3",
-          "children": [
-            {
-              "name": "chrMT-10237-C",
-              "parent": "ND3",
-              "color": "#8c564b",
-              "lightcolor": "#c49c94",
-              "type": "mutation",
-              "link": "http://www.ncbi.nlm.nih.gov/snp/193302927"
-            }
-            ]},
-            {
-          "name": "ND4",
-          "parent": "Leber's Optic Atrophy",
-          "color": "#e377c2",
-          "lightcolor": "#f7b6d2",
-          "type": "gene",
-          "link": "https://en.m.wikipedia.org/wiki/MT-ND4",
-          "children": [
-            {
-              "name": "chrMT-11253-C",
-              "parent": "ND4",
-              "color": "#e377c2",
-              "lightcolor": "#f7b6d2",
-              "type": "mutation",
-              "link": "http://www.ncbi.nlm.nih.gov/snp/200145866"
-            }
-            ]},
-      {
-        "name": "ND5",
-        "parent": "Leber's Optic Atrophy",
-        "color": "#9467bd",
-        "lightcolor": "#c5b0d5",
-        "type": "gene",
-        "link": "https://en.m.wikipedia.org/wiki/MT-ND5",
-        "children": [
-          {
-            "name": "chrMT-13708-A",
-            "parent": "ND5",
-            "color": "#9467bd",
-            "lightcolor": "#c5b0d5",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/28359178"
-          },
-          {
-            "name": "chrMT-12811-C",
-            "parent": "ND5",
-            "color": "#9467bd",
-            "lightcolor": "#c5b0d5",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/199974018"
-          },
-          {
-            "name": "chrMT-12338-C",
-            "parent": "ND5",
-            "color": "#9467bd",
-            "lightcolor": "#c5b0d5",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/201863060"
-          },
-          {
-            "name": "chrMT-13637-G",
-            "parent": "ND5",
-            "color": "#9467bd",
-            "lightcolor": "#c5b0d5",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/200855215"
-          }]},
-          {
-        "name": "COX3",
-        "parent": "Leber's Optic Atrophy",
-        "color": "#7f7f7f",
-        "lightcolor": "#c7c7c7",
-        "type": "gene",
-        "link": "https://en.m.wikipedia.org/wiki/COX-3",
-        "children": [
-          {
-            "name": "chrMT-9438-A",
-            "parent": "COX3",
-            "color": "#7f7f7f",
-            "lightcolor": "#c7c7c7",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/267606611"
-          },
-          {
-            "name": "chrMT-9804-A",
-            "parent": "COX3",
-            "color": "#7f7f7f",
-            "lightcolor": "#c7c7c7",
-            "type": "mutation",
-            "link": "http://www.ncbi.nlm.nih.gov/snp/200613617"
-          }]}
-    ]
-  }
-];
+var tooltip = d3.select('#viz2').append('div')
+       .style('position','absolute') //To allow d3 to follow the position absolute to the relationship to the page
+       .style('padding','0 10px') //To do padding on the toop tip. 0 on the top and bottom and 10px on each side
+       .style('background','white')
+       .style('opacity',0); // 0 as we don't want to show when the graphic first loads up
 
+var x0 = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
-var margin = {top: 0, right: 20, bottom: 0, left: 200},
-  width = 960 - margin.right - margin.left,
-  height = 500 - margin.top - margin.bottom;
-  
-var i = 0,
-  duration = 750,
-  root;
-var tree = d3.layout.tree()
-  .size([height, width]);
-var diagonal = d3.svg.diagonal()
-  .projection(function(d) { return [d.y, d.x]; });
-var svg = d3.select("#viz2").append("svg")
-  .attr("width", width + margin.right + margin.left)
-  .attr("height", height + margin.top + margin.bottom)
+var x1 = d3.scale.ordinal();
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var color = d3.scale.ordinal()
+    .range(["#8a89a6", "#d0743c"]);
+
+var xAxis = d3.svg.axis()
+    .scale(x0)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(d3.format(".2s"));
+
+var svg2 = d3.select("#viz2").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.call(tip);
+d3.csv("data/a.csv", function(error, data) {
+  var categories = d3.keys(data[0]).filter(function(key) { return key !== "Disease" && key.indexOf('num') == -1; });
+  var numbers = d3.keys(data[0]).filter(function(key) { return key.indexOf('num') != -1; });
 
-root = leber[0];
-root.x0 = height / 2;
-root.y0 = 0;
-  
-update(root);
-
-d3.select('#opts')
-  .on('change', function() {
-    var newData = eval(d3.select(this).property('value'));
-    root = newData[0];
-    update(root);
-});
-
-d3.select(self.frameElement).style("height", "500px");
-function update(source) {
-  // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse(),
-    links = tree.links(nodes);
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 100; });
-  // Update the nodes…
-  var node = svg.selectAll("g.node")
-    .data(nodes, function(d) { return d.id || (d.id = ++i); });
-  // Enter any new nodes at the parent's previous position.
-  var nodeEnter = node.enter().append("g")
-    .attr("class", "node")
-    .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-    .on("click", click);
-  nodeEnter.append("circle")
-    .attr("r", 1e-6)
-    .style("fill", function(d) { return d._children ? "orchid" : "#fff"; });
-  nodeEnter.append("text")
-    .attr("x", function(d) { return d.children || d._children ? -13 : 13; })
-    .attr("dy", ".35em")
-    .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-    .text(function(d) { return d.name; })
-    .style("fill-opacity", 1e-6);
-
-  // Transition nodes to their new position.
-  var nodeUpdate = node.transition()
-    .duration(duration)
-    .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-  nodeUpdate.select("circle")
-    .attr("r", 10)
-    .style("stroke", function(d) { return d.color;})
-    .style("fill", function(d) { return d._children ? d.lightcolor : "#fff"; });
-  nodeUpdate.select("text")
-    .style("fill-opacity", 1);
-  // Transition exiting nodes to the parent's new position.
-  var nodeExit = node.exit().transition()
-    .duration(duration)
-    .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-    .remove();
-  nodeExit.select("circle")
-    .attr("r", 1e-6);
-  nodeExit.select("text")
-    .style("fill-opacity", 1e-6);
-  // Update the links…
-  var link = svg.selectAll("path.link")
-    .data(links, function(d) { return d.target.id; });
-  // Enter any new links at the parent's previous position.
-  link.enter().insert("path", "g")
-    .attr("class", "link")
-    .attr("d", function(d) {
-    var o = {x: source.x0, y: source.y0};
-    return diagonal({source: o, target: o});
-    });
-  // Transition links to their new position.
-  link.transition()
-    .duration(duration)
-    .attr("d", diagonal);
-  // Transition exiting nodes to the parent's new position.
-  link.exit().transition()
-    .duration(duration)
-    .attr("d", function(d) {
-    var o = {x: source.x, y: source.y};
-    return diagonal({source: o, target: o});
-    })
-    .remove();
-  // Stash the old positions for transition.
-  nodes.forEach(function(d) {
-  d.x0 = d.x;
-  d.y0 = d.y;
+  data.forEach(function(d) {
+    d.percents = categories.map(function(name) { return {name: name, value: +d[name]}; });
+    d.numbers = numbers.map(function(name) { tn = name.split('_')[0]; return {"num_name": tn, "num_value": +d[name]}; });
+    arr = []
+    for (var key in d.numbers) {
+      nv = []
+      n = d.numbers[key].num_name;
+      for (var pkey in d.percents) {
+        pn = d.percents[pkey].name;
+        if (pn == n) {
+          v = d.percents[pkey].value;
+          nv.push(v);
+          nv.push(d.numbers[key].num_value);
+          arr.push({name: n, value: nv})
+          break;
+        }
+      }
+    }
+    d.percents = arr;
   });
 
-  svg.selectAll(".node")
-  .select("text")
-  .on('mouseover', tip.show);
-}
-// Toggle children on click.
-function click(d) {
-  if (d.children) {
-  d._children = d.children;
-  d.children = null;
-  } else {
-  d.children = d._children;
-  d._children = null;
-  }
-  update(d);
+  x0.domain(data.map(function(d) { return d.Disease; }));
+  x1.domain(categories).rangeRoundBands([0, x0.rangeBand()]);
+  y.domain([0, d3.max(data, function(d) { return d3.max(d.percents, function(d) { return d.value[0]; }); })]);
 
-}
-var h = .2;
-//width of each column in the heatmap
-var w = 7;
+  svg2.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
-//attach a SVG node to the document
-//height and width defined by the row/column lengths
-var mySVG = d3.select("body")
-  .append("svg")
-  .attr("width", (w * cols.length) + 400) 
-  .attr("height", (h * rows.length + 100))
-  .style('position','absolute')
-  .style('top',0)
-  .style('left',0);
+  svg2.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr('class', 'y label')
+      .attr("transform", "rotate(-90)")
+      .attr("y", 2)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Percent");
 
-//define a color scale using the min and max expression values
-var colorScale = d3.scale.linear()
-  .domain([minData, 0, maxData])
-  .range(["blue", "white", "red"]);
+  var disease = svg2.selectAll(".disease")
+      .data(data)
+    .enter().append("g")
+      .attr("class", "g")
+      .attr("transform", function(d) { return "translate(" + x0(d.Disease) + ",0)"; })
+      .on("click", function(d) {
+         console.log(d.Disease)
+      });
 
-//generate the heatmap
-var heatmapRow = mySVG.selectAll(".heatmap")
-  .data(data)
-  .enter().append("g");
+  disease.selectAll("rect")
+      .data(function(d) { return d.percents; })
+    .enter().append("rect")
+      .attr("width", x1.rangeBand())
+      .attr("x", function(d) { return x1(d.name); })
+      .attr("y", function(d) { return y(d.value[0]); })
+      .attr("height", function(d) { return height - y(d.value[0]); })
+      .style("fill", function(d) { return color(d.name); })
+      .on("mouseover", function(d) {
+         tooltip.transition()
+            .style('opacity', .9);
+         tooltip.html(d.value[0] + "% of " + d.value[1])
+            .style('left', (d3.event.pageX - 15) + 'px') //position of the tooltip
+            .style('top', (d3.event.pageY - 20) + 'px');
+
+         d3.select(this)
+            .style('opacity',.5)
+      })
+      //To reset the color, hence opacity = 1
+      .on('mouseout',function(d){
+         tooltip.transition().style('opacity', 0)
+         d3.select(this)
+            .style('opacity',1)
+      });
+
+  disease.selectAll('tick')
+      .on('click', function(d) {
+         console.log(d)
+      });
+
+  var legend = svg2.selectAll(".legend")
+      .data(categories.slice().reverse())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
+
+});
+
+
