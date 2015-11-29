45fc732a3d062288a77c10bcfa8b652144b240a0
Nathaniel Black <nathaniel.black@ischool.berkeley.edu>
2015-11-29 14:53:25 -0500

    heatmap test

---
+++ js/heatmap.js
@@ -0,0 +1,87 @@
+var margin = { top: 50, right: 50, bottom: 100, left: 50 },
+  cellSize=5;
+  col_number=111;
+  row_number=88;
+  width1 = cellSize*col_number, 
+  height1 = cellSize*row_number , 
+  legendElementWidth = cellSize*10,
+  colorBuckets = 21,
+  colors = ['#d62728','#ff9896','#aec7e8','#1f77b4'];
+
+var dataBlanks = [];
+  for (var i = 0; i < (row_number * col_number); i++) {
+    dataBlanks[i] = 0; 
+    console.log(i);}
+
+d3.csv("gbr_heatmap.csv",
+function(d) {
+  return {
+    row:   +d.row,
+    col:   +d.col,
+    value: +d.Severity,
+    Mut: d.Mut,
+    Sample: d.Sample
+  };
+},
+function(error, data) {
+  var colorScale = d3.scale.quantile()
+      .domain([ -2, 0, 2])
+      .range(colors);
+  
+  var svg1 = d3.select("#viz1").append("svg")
+      .attr("width", width1 + margin.left + margin.right)
+      .attr("height", height1 + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      ;
+  var rowSortOrder=false;
+  var colSortOrder=false;
+
+
+ var heatMap = svg1.append("g").attr("class","g3")
+        .selectAll(".cellg")
+        .data(data,function(d){return d.row+":"+d.col;})
+        .enter()
+        .append("rect")
+        .attr("x", function(d) { return d.col * cellSize; })
+        .attr("y", function(d) { return d.row * cellSize; })
+        .attr("class", function(d){return "cell cell-border cr"+(d.row-1)+" cc"+(d.col-1);})
+        .attr("width", cellSize)
+        .attr("height", cellSize)
+        .attr("fill", function(d) { return colorScale(d.value); })
+        .on("mouseover", function(d){
+               d3.select(this).classed("cell-hover",true);
+
+               d3.select("#tooltip")
+                 .style("left", (d3.event.pageX+10) + "px")
+                 .style("top", (d3.event.pageY-10) + "px")
+                 .select("#value")
+                 .text("\nMutation:"+d.Mut+"\nPerson:"+d.Sample);  
+               d3.select("#tooltip").classed("hidden", false);
+        })
+        .on("mouseout", function(){
+               d3.select(this).classed("cell-hover",false);
+               d3.select("#tooltip").classed("hidden", true);
+        })
+        ;
+
+  svg1.append("text")
+    .attr("class", "x label")
+    .attr("x", width1)
+    .attr("y", height1 + margin.bottom-5)
+    .attr("text-anchor", "end")
+    .text("Position on Chromosome")
+    .style("font", "sans-serif")
+    .style("font-size","12px");
+
+  svg1.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("Individual in Population")
+    .style("font", "sans-serif")
+    .style("font-size","12px");
+  
+});
