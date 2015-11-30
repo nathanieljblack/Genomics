91603ad15f3fdb5bed3cb88a4f521b91606d1fe6
Nathaniel Black <nathaniel.black@ischool.berkeley.edu>
2015-11-29 21:04:20 -0500

    nate2.html

--- js/viz2.js
+++ js/viz2.js
@@ -849,7 +849,7 @@ var root = BC[0];
 function updateBarChart(pop) {
 
   var margin2 = {top: 20, right: 20, bottom: 20, left: 50},
-      width2 = 650 - margin2.left - margin2.right,
+      width2 = 600 - margin2.left - margin2.right,
       height2 = 600 - margin2.top - margin2.bottom;
 
   var tooltip = d3.select('#viz2Right').append('div')
