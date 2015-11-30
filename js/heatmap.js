34304adc2765da4b2cb984920da44d822df5b78a
Nathaniel Black <nathaniel.black@ischool.berkeley.edu>
2015-11-29 21:01:23 -0500

    nate2.html

--- js/heatmap.js
+++ js/heatmap.js
@@ -16,7 +16,7 @@ var popRows =
         'PUR': 94};
 
 function updateHeatMap(pop) {
-  var margin = { top: 20, right: 20, bottom: 50, left: 10 },
+  var margin = { top: 50, right: 20, bottom: 20, left: 10 },
     cellSize=5;
     col_number=111;
     row_number= popRows[pop];
